8692e9a1f332dd61a94343dc67b9f8723a839c56
jesus <jesusnazi8888@gmail.com>
2022-07-20 16:31:16 +0300

    +Class_Graph

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,12 @@
 #include "SDL.h"
 #include <iostream>
+#include "Class_Graph.h"
 
 int main(int argc, char* argv[])
 {
-    while (true)
-    {
-        std::cout << "hh\n";
-    }
+    Graph g = Graph();
+    for (int i = 0; i < 5; i++)
+        std::cout << g.A[i]<<"\n";
 
     return EXIT_SUCCESS;
 }
