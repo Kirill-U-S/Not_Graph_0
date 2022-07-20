496a082ffcd19183f598eee35fbf37f087c7c6f5
jesus <jesusnazi8888@gmail.com>
2022-07-20 14:56:03 +0300

    2

--- Main.cpp
+++ Main.cpp
@@ -10,10 +10,8 @@ int main(int argc, char* argv[])
 
     SDL_Window* window = SDL_CreateWindow("Hello SDL World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
 
-    // Check that the window was successfully created
     if (NULL == window)
     {
-        // In the case that the window could not be made...
         std::cout << "Could not create window: " << SDL_GetError() << std::endl;
         return 1;
     }
@@ -23,12 +21,8 @@ int main(int argc, char* argv[])
     while (true)
     {
         if (SDL_PollEvent(&windowEvent))
-        {
             if (SDL_QUIT == windowEvent.type)
-            {
                 break;
-            }
-        }
     }
 
     SDL_DestroyWindow(window);
